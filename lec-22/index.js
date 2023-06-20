fb8bac25b8ba5faac21cecbda0bcfa74a7abe2be
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-06-20 14:34:06 +0530

    Advance-javaScript

---
+++ lec-22/index.js
@@ -0,0 +1,69 @@
+/*
+
+Encapsulation in javascript : 
+Abstraction in javascript : 
+
+Polymorphism :
+compile Time Polymorphism :- Method overloading
+Run Time Polymorphism :- Mothod overriding 
+
+
+*/
+
+class user{
+    name ;
+    email ;   
+    #password;
+    constructor(){
+        console.log("This is default constructor");
+    }
+    #validateEmail(email){
+        return true ;  
+    }
+    #validatepassword(password){
+        return true;
+    }
+    signup (name,email,password){
+        let isvalidate = flase;
+        isvalidate &&=this.#validateEmail(email);  
+        isvalidate &&=this.#validatepassword(password);
+        if(isvalidate){
+            this.name=name;
+            this.email=email;
+            this.#password=password;
+            console.log("user Added successfully");
+        }else{
+            console.log("please Enter details again!!");
+        }
+    }
+    login(email,password){
+        if (email===this.email && password=== this.#password){
+            console.log("Login successfully");
+        }else{
+            console.log("Authentication failed");
+        }
+    } 
+    #isRegistereduser(email){
+        return true;
+    }
+    resetpassword(email,newpassword){
+        if(this.#isRegistereduser(email)){
+            this.#password=newpassword;
+            console.log("operation performed successfully");
+        }else{ 
+            console.log("No account found");  
+        }
+    }
+} 
+
+constuser = new user ();
+user.signup("viren",'viren@gmail.com','123456');
+// user.#validateEmail("viren@gmail.com");
+// user.login("viren@outlook.com",'123456');
+// user.login("viren@gmail.com",'123456'); 
+// user.resetpassword("viren@gmail.com","viren"); 
+// user.login("viren@gmail.com",'123456');
+// user.reSetpassword("viren@outlook.com","viren");    
+
+
+
