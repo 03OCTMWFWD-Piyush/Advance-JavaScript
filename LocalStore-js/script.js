d31f1e3f9be4fdf1c72a3eba7ecb6b82fbddfd7d
03OCTMWFWD-Piyush <piyushdhunt8980@gmail.com>
2023-07-16 14:09:08 +0530

    HTML

--- LocalStore-js/script.js
+++ LocalStore-js/script.js
@@ -1,19 +1,19 @@
-let form=document.getElementById('form'); 
+let form = document.getElementById('form');
 
-form.addEventListener('submit', (e) =>{
-e.preventDefult();
-let username = document.getElementById('username').value;
-let passsword = document.getElementById('password').value;
+form.addEventListener('submit', (e) => {
+    e.preventDefult();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
 
-// local storage ;-
+    // local storage ;-
 
-localStorage.setItem('username',username);
-localStorage.setItem('password',passsword);
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
 
-alert("Data Submitted Successfully");
-});        
+    alert("Data Submitted Successfully");
+}); 
 
-if(localStorage.getItem('username')&& localStorage.getItem('password')){
-    document.getElementById('username').value=localStorage.getItem('username');
-    document.getElementById('password').value=localStorage.getItem('password');
-};     
+// if(localStorage.getItem('username')&& localStorage.getItem('password')){
+//     document.getElementById('username').value=localStorage.getItem('username');
+//     document.getElementById('password').value=localStorage.getItem('password');
+// };         
